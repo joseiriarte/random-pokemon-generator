24a28106bb95288110ecdaa09be5218aae62da82
Jose Iriarte <joseiriartedev@gmail.com>
2024-09-17 17:50:02 -0300

    Converted image from png to webp

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,5 @@
 import github from './github.svg';
-import pikachu from './pikachu.png';
+import pikachu from './pikachu.webp';
 import pokeball from './pokeball.svg';
 
 export { github, pikachu, pokeball };
